169ddeb52c4cf7632b5f778d8efbd62502558b4f
erick20212 <erick.quichua@upeu.edu.pe>
2024-11-08 00:45:54 -0500

    back mejorado

--- src/main/java/upeu/daoimpl/CocheDaoImpl.java
+++ src/main/java/upeu/daoimpl/CocheDaoImpl.java
@@ -8,42 +8,59 @@ import org.springframework.stereotype.Component;
 
 import upeu.dao.CocheDao;
 import upeu.entity.Coche;
+import upeu.entity.Marca;
+import upeu.entity.Tipo;
 import upeu.repository.CocheRepository;
+import upeu.repository.MarcaRepository;
+import upeu.repository.TipoRepository;
 
 @Component
-public class CocheDaoImpl implements CocheDao{
-	
-	@Autowired
-	private CocheRepository repository;
-	@Override
-	public Coche create(Coche a) {
-		// TODO Auto-generated method stub
-		return repository.save(a);
-	}
+public class CocheDaoImpl implements CocheDao {
 
-	@Override
-	public Coche update(Coche a) {
-		// TODO Auto-generated method stub
-		return repository.save(a);
-	}
+    @Autowired
+    private CocheRepository repository;
 
-	@Override
-	public void delete(Long id) {
-		// TODO Auto-generated method stub
-		repository.deleteById(id);
-		
-	}
+    @Autowired
+    private MarcaRepository marcaRepository;
 
-	@Override
-	public Optional<Coche> read(Long id) {
-		// TODO Auto-generated method stub
-		return repository.findById(id);
-	}
+    @Autowired
+    private TipoRepository tipoRepository;
 
-	@Override
-	public List<Coche> readAll() {
-		// TODO Auto-generated method stub
-		return repository.findAll();
-	}
+    @Override
+    public Coche create(Coche coche) {
+        // Cargar la entidad Marca usando el ID proporcionado en coche
+        Marca marca = marcaRepository.findById(coche.getMarca().getId())
+                .orElseThrow(() -> new RuntimeException("Marca no encontrada"));
 
+        // Cargar la entidad Tipo usando el ID proporcionado en coche
+        Tipo tipo = tipoRepository.findById(coche.getTipo().getId())
+                .orElseThrow(() -> new RuntimeException("Tipo no encontrado"));
+
+        // Asignar las relaciones de Marca y Tipo a la entidad Coche
+        coche.setMarca(marca);
+        coche.setTipo(tipo);
+
+        // Guardar la entidad en la base de datos
+        return repository.save(coche);
+    }
+
+    @Override
+    public Coche update(Coche coche) {
+        return repository.save(coche);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Coche> read(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Coche> readAll() {
+        return repository.findAll();
+    }
 }
